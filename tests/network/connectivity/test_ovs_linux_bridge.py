c1295989bac266befe102025080ef1ab446cbac7
rnetser <rnetser@redhat.com>
2025-08-03 10:50:22 +0300

    remove conformance marker from test_ipv4_linux_bridge

--- tests/network/connectivity/test_ovs_linux_bridge.py
+++ tests/network/connectivity/test_ovs_linux_bridge.py
@@ -29,7 +29,6 @@ class TestConnectivityLinuxBridge:
     )
     @pytest.mark.ipv4
     @pytest.mark.s390x
-    @pytest.mark.conformance
     def test_ipv4_linux_bridge(
         self,
         use_default_bridge,
