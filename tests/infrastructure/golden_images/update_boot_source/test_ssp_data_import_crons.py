8e42f35e3a2018937ffc3bdaeecd31c65a3d7b63
Roni Kishner <87259550+RoniKishner@users.noreply.github.com>
2026-01-14 18:29:53 +0200

    [4.18] Change method of testing latest rhel versions used in data source (#3393)
    ##### Short description:
    Change method of testing latest rhel versions used in data source
    
    ##### More details:
    Manual cherry pick for
    https://github.com/RedHatQE/openshift-virtualization-tests/pull/3241
    
    ##### What this PR does / why we need it:
    Remove use of osdb-info on tests
    
    Signed-off-by: rkishner <rkishner@redhat.com>

--- tests/infrastructure/golden_images/update_boot_source/test_ssp_data_import_crons.py
+++ tests/infrastructure/golden_images/update_boot_source/test_ssp_data_import_crons.py
@@ -17,6 +17,7 @@ from tests.infrastructure.golden_images.constants import (
     DEFAULT_FEDORA_REGISTRY_URL,
 )
 from tests.infrastructure.golden_images.update_boot_source.utils import (
+    get_image_version,
     template_labels,
 )
 from utilities.constants import BIND_IMMEDIATE_ANNOTATION, TIMEOUT_1MIN, TIMEOUT_2MIN, TIMEOUT_5MIN, TIMEOUT_10MIN
@@ -497,3 +498,33 @@ def test_data_source_instancetype_preference_label(
     assert not failed_data_source_list, (
         f"Could not create VMs with the following data sources: {failed_data_source_list}"
     )
+
+
+@pytest.mark.parametrize(
+    "rhel_version",
+    [
+        pytest.param("rhel8", id="rhel8"),
+        pytest.param("rhel9", id="rhel9"),
+        pytest.param("rhel10", id="rhel10"),
+    ],
+)
+@pytest.mark.polarion("CNV-12414")
+def test_updated_rhel_image(
+    admin_client,
+    golden_images_namespace,
+    latest_rhel_release_versions_dict,
+    rhel_version,
+):
+    rhel_dic = DataImportCron(
+        client=admin_client,
+        name=f"{rhel_version}-image-cron",
+        namespace=golden_images_namespace.name,
+        ensure_exists=True,
+    )
+    rhel_instance_dict = rhel_dic.instance
+    image_reference_version = get_image_version(
+        image=rhel_instance_dict.metadata.annotations.get("cdi.kubevirt.io/storage.import.imageStreamDockerRef")
+    )
+    managed_data_source = rhel_instance_dict.spec.managedDataSource
+    assert managed_data_source, "spec.managedDataSource doesn't exists"
+    assert latest_rhel_release_versions_dict[managed_data_source] == image_reference_version
