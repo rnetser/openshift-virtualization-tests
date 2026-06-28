8343a862dd043341fd525ffb12c487e1b3af78e9
Den Shchedrivyi <dshchedr@redhat.com>
2026-06-28 00:16:10 -0700

    [VIRT] Re-enabling test_cloud_init_disk_vm (#5404)
    Bug fixed, re-enabling test_cloud_init_disk_vm
    
    ##### What this PR does / why we need it:
    
    ##### Which issue(s) this PR fixes:
    
    ##### Special notes for reviewer:
    
    ##### jira-ticket:
    <!-- full-ticket-url needs to be provided. This would add a link to the
    pull request to the jira and close it when the pull request is merged
    If the task is not tracked by a Jira ticket, just write "NONE".
    -->
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **Chores**
    * Updated a test tag used by the automated test suite, changing how the
    test is categorized and selected.
    
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    Signed-off-by: Denys Shchedrivyi <dshchedr@redhat.com>

--- tests/virt/node/general/test_cloud_init_disk_vm.py
+++ tests/virt/node/general/test_cloud_init_disk_vm.py
@@ -18,7 +18,6 @@ def vm_with_cloud_init_disk(namespace):
 
 
 @pytest.mark.polarion("CNV-10555")
-@pytest.mark.jira("CNV-87521", run=False)
 def test_vm_with_cloud_init_disk_logging_no_disk_capacity(vm_with_cloud_init_disk, admin_client):
     assert "No disk capacity" not in vm_with_cloud_init_disk.vmi.get_virt_launcher_pod(
         privileged_client=admin_client
